17a11f6530a70856d10d7179b9fe172ee2a74f99
naheller <naheller@gmail.com>
2025-03-20 13:15:37 +0530

    add mobile view

--- generateHtml.js
+++ generateHtml.js
@@ -2,27 +2,82 @@ const fs = require("fs");
 const { fetchSpreadsheetData } = require("./googleSheet");
 const {
   getHtmlWithContentOrError,
-  responseDataToTableHtmlString,
+  responseDataToDesktopHtml,
+  responseDataToMobileHtml,
 } = require("./htmlPieces");
 
 function getContentFromSpreadsheetData(responseData) {
   // Handle error message
   if (typeof responseData === "string") {
-    return `
-      <p>${responseData}</p>
-    `;
+    return {
+      mobile: `<p>${responseData}</p>`,
+      desktop: `<p>${responseData}</p>`,
+    };
   }
 
-  const contentHtml = responseDataToTableHtmlString(responseData);
-  return contentHtml;
+  const desktopContentHtml = responseDataToDesktopHtml(responseData);
+  const mobileContentHtml = responseDataToMobileHtml(responseData);
+
+  return {
+    mobile: mobileContentHtml,
+    desktop: desktopContentHtml,
+  };
 }
 
 async function generateHtmlFile() {
   const spreadsheetData = await fetchSpreadsheetData();
-  const contentHtml = getContentFromSpreadsheetData(spreadsheetData);
-  const htmlString = getHtmlWithContentOrError(contentHtml);
 
-  fs.writeFile("./index.html", htmlString, (err) => {
+  const spreadsheetDataShroot = {
+    ...spreadsheetData,
+    values: [
+      spreadsheetData.values[0],
+      ...spreadsheetData.values.slice(1).filter((row) => row[1] === "Shroot"),
+    ],
+  };
+
+  const spreadsheetDataNoot = {
+    ...spreadsheetData,
+    values: [
+      spreadsheetData.values[0],
+      ...spreadsheetData.values.slice(1).filter((row) => row[1] === "Noot"),
+    ],
+  };
+
+  const { mobile, desktop } = getContentFromSpreadsheetData(spreadsheetData);
+  const { mobile: mobileShroot, desktop: desktopShroot } =
+    getContentFromSpreadsheetData(spreadsheetDataShroot);
+  const { mobile: mobileNoot, desktop: desktopNoot } =
+    getContentFromSpreadsheetData(spreadsheetDataNoot);
+
+  const html = getHtmlWithContentOrError(mobile, desktop);
+  const htmlShroot = getHtmlWithContentOrError(
+    mobileShroot,
+    desktopShroot,
+    "Shroot's Books"
+  );
+  const htmlNoot = getHtmlWithContentOrError(
+    mobileNoot,
+    desktopNoot,
+    "Noot's Books"
+  );
+
+  fs.writeFile("./index.html", html, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // File written successfully
+    }
+  });
+
+  fs.writeFile("./shroot.html", htmlShroot, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // File written successfully
+    }
+  });
+
+  fs.writeFile("./noot.html", htmlNoot, (err) => {
     if (err) {
       console.error(err);
     } else {
--- htmlPieces.js
+++ htmlPieces.js
@@ -1,59 +1,185 @@
-function getHtmlWithContentOrError(contentOrError) {
+function getHtmlWithContentOrError(
+  mobileContentOrError,
+  desktopContentOrError,
+  customTitle = "Book Tracker"
+) {
   return `
     <!DOCTYPE html>
     <html lang="en">
       <head>
-        <title>Book Tracker</title>
+        <title>${customTitle}</title>
         <link rel="stylesheet" href="styles.css" />
       </head>
       <body>
-        <h1>Book Tracker</h1>
-        <div id="content">${contentOrError}</div>
+        <h1>${customTitle}</h1>
+        <div class="nav-button-group">
+          <button onclick="window.location.pathname='/';">All books</button>
+          <button onclick="window.location.pathname='/shroot.html';">Shroot</button>
+          <button onclick="window.location.pathname='/noot.html';">Noot</button>
+        </div>
+        <div id="desktop-content">${desktopContentOrError}</div>
+        <div id="mobile-content">${mobileContentOrError}</div>
       </body>
     </html>
   `;
 }
 
-function responseDataToTableHtmlString(responseData) {
-  const rowHeaders = responseData.values[0];
-  const rowValues = responseData.values.slice(1);
+function responseDataToDesktopHtml(responseData) {
+  const [timestamp, goober, title, author, genre, rating] =
+    responseData.values[0];
 
-  const allRowsHtmlString = rowValues
-    .map((row) => rowDataToHtmlString(row))
-    .join("");
+  const rowValues = responseData.values.slice(1);
+  const sortedRowValues = rowValues.sort(
+    (a, b) => new Date(a).getTime() - new Date(b).getTime()
+  );
+
+  const allRowsHtml = sortedRowValues.map((row) => rowDataToHtml(row)).join("");
 
   return `
     <table>
       <tr>
-        <th>${rowHeaders[0]}</th>
-        <th>${rowHeaders[1]}</th>
-        <th>${rowHeaders[2]}</th>
-        <th>${rowHeaders[3]}</th>
-        <th>${rowHeaders[4]}</th>
-        <th>${rowHeaders[5]}</th>
+        <th>Date</th>
+        <th>${goober}</th>
+        <th>${title}</th>
+        <th>${author}</th>
+        <th>${genre}</th>
+        <th>${rating}</th>
       </tr>
-      ${allRowsHtmlString}
+      ${allRowsHtml}
     </table>
   `;
 }
 
-function rowDataToHtmlString(rowData) {
+function rowDataToHtml(rowData) {
   const [timestamp, goober, title, author = "", genre = "", rating = ""] =
     rowData;
 
+  const date = new Date(timestamp);
+  const formattedDate = date.toLocaleDateString("en-us", {
+    month: "long",
+    year: "numeric",
+  });
+
   return `
     <tr>
-      <td>${timestamp}</td>
+      <td>${date.toLocaleDateString()}</td>
       <td>${goober}</td>
       <td>${title}</td>
       <td>${author}</td>
       <td>${genre}</td>
-      <td>${rating}</td>
+      <td>${rating ? ratingNumToStars(Number(rating)) : ""}</td>
     </tr>
+  `;
+}
+
+function getFormattedResponseData(rawResponseData) {
+  const headers = rawResponseData.values[0];
+  const formattedResponseData = rawResponseData.values.splice(1).map((row) => ({
+    ...headers.reduce(
+      (acc, curr, index) => ({
+        ...acc,
+        [curr]: row[index] || "",
+      }),
+      {}
+    ),
+  }));
+  return formattedResponseData;
+}
+
+function ratingNumToStars(ratingNum) {
+  const filledStar = "★";
+  const emptyStar = "☆";
+  const maxRating = 5;
+
+  const stars = Array(ratingNum)
+    .fill(filledStar)
+    .concat(Array(maxRating - ratingNum).fill(emptyStar))
+    .join("");
+
+  return stars;
+}
+
+function rowDataToBookCardHtml(rowData) {
+  const { Timestamp, Goober, Title, Author, Genre, Rating } = rowData;
+  // const formattedDate = new Date(Timestamp).toLocaleString("default", {
+  //   month: "long",
+  // });
+  return `
+    <div class="book-card">
+      <p class="title">${Title}</p>
+      <p class="author">by ${Author || "Unknown"}</p>
+      <p class="genre">${Genre}</p>
+      <p class="rating">${Rating ? ratingNumToStars(Number(Rating)) : ""}</p>
+    </div>
+  `;
+}
+
+function responseDataToMobileHtml(responseData) {
+  const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const formattedData = getFormattedResponseData(responseData);
+  const formattedDataByYearMonth = formattedData.reduce((acc, curr) => {
+    const date = new Date(curr.Timestamp);
+    const year = date.getFullYear();
+    const monthNum = date.getMonth();
+
+    if (acc[year]?.[monthNum]) {
+      acc[year][monthNum].push(curr);
+    } else {
+      acc[year] = {
+        ...acc[year],
+        [monthNum]: [curr],
+      };
+    }
+    return acc;
+  }, {});
+
+  return `
+    <div>
+      ${Object.keys(formattedDataByYearMonth)
+        .sort((yearA, yearB) => Number(yearB) - Number(yearA))
+        .map(
+          (year) =>
+            `
+          <div>
+            <h2>${year}</h2>
+            <div>
+              ${Object.keys(formattedDataByYearMonth[year])
+                .sort((monthA, monthB) => Number(monthB) - Number(monthA))
+                .map(
+                  (monthNum) => `
+                    <div>
+                      <h4 class="month-header">${MONTHS[monthNum]}</h4>
+                      ${formattedDataByYearMonth[year][monthNum]
+                        .map((book) => rowDataToBookCardHtml(book))
+                        .join("")}
+                    </div>
+                  `
+                )
+                .join("")}
+            </div>
+          </div>
+        `
+        )
+        .join("")}
+    </div>
   `;
 }
 
 module.exports = {
   getHtmlWithContentOrError,
-  responseDataToTableHtmlString,
+  responseDataToDesktopHtml,
+  responseDataToMobileHtml,
 };
